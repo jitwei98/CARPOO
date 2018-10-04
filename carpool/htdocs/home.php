4371d8d4ea40380bf9a1e6b11bb86aacfb6b0a94
kelvintankaiboon <e0202641@u.nus.edu>
2018-10-04 23:25:26 +0800

    Copy carpool folder into C:\Bitnami\wappstack-7.1.21-0\apps, edit conf files within and conf files in C:\Bitnami\wappstack-7.1.21-0\apache2\conf\bitnami

---
+++ carpool/htdocs/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<style>
+			a {
+				text-decoration: none;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="w3-container w3-black">
+		  <a href="/carpool/home"><h1>Car Pooling</h1></a>
+		</div>
+		<div class="w3-container">
+			<h1>here is carpool</h1>
+		</div>
+	</body>
+</html>
