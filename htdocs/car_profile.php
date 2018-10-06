3eb5aacf55a73b837ddfab8207c044387f6c52b1
jitwei98 <limjitwei@gmail.com>
2018-10-06 15:46:26 +0800

    Resolve merge conflict on car_profile.php

--- htdocs/car_profile.php
+++ htdocs/car_profile.php
@@ -1,13 +1,13 @@
 <?php   session_start();  ?>
 <?php
   if(!isset($_SESSION['use'])) // If session is not set then redirect to Login Page
-<<<<<<< HEAD
   {
   	header("Location: /carpool");  
   }
   $db = pg_connect("host=localhost port=5432 dbname=carpool user=postgres password=test");
   $driver = $_SESSION['use'];
   ?>
+
   <!DOCTYPE html>
   <html>
   <head>
@@ -25,9 +25,9 @@
 		<a href="logout.php" style="float:right;">Log Out</a>
 	</div>
 	<div class="w3-sidebar w3-bar-block w3-dark-gray" style="width:10%"> 
-		<a href="#" class="w3-bar-item w3-button">Initiate Car Pool</a>
-		<a href="#" class="w3-bar-item w3-button">Car Profile</a>
-		<a href="#" class="w3-bar-item w3-button">Driver Profile</a>
+		<a href="/carpool/offer_form" class="w3-bar-item w3-button">Initiate Car Pool</a>
+		<a href="/carpool/car_profile" class="w3-bar-item w3-button">Car Profile</a>
+		<a href="/carpool/driver_profile" class="w3-bar-item w3-button">Driver Profile</a>
 		<a href="#" class="w3-bar-item w3-button">Car Pool History</a>
 	</div>
 	<div style="margin-left: 10%">
@@ -106,38 +106,8 @@
 				}
 				?>
 			</h1>
-=======
-   {
-       header("Location: /carpool");  
-   }
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		<style>
-			a {
-				text-decoration: none;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="w3-container w3-black">
-			<a href="/carpool/home"><h1>Car Pooling</h1></a>
-  			<a href="logout.php" style="float:right;">Log Out</a>
->>>>>>> f2706eb1273791b2544c041269287e9709070158
+			?>
 		</div>
-		<div class="w3-sidebar w3-bar-block w3-dark-gray" style="width:10%"> 
-		  <a href="/carpool/offer_form" class="w3-bar-item w3-button">Initiate Car Pool</a>
-		  <a href="/carpool/car_profile" class="w3-bar-item w3-button">Car Profile</a>
-		  <a href="/carpool/driver_profile" class="w3-bar-item w3-button">Driver Profile</a>
-		  <a href="#" class="w3-bar-item w3-button">Car Pool History</a>
-		</div>
-		<div style="margin-left: 10%">
-			<div class="w3-container">
-				<h1>Car Profile</h1>
-			</div>
-		</div>
-	</body>
+	</div>
+</body>
 </html>
