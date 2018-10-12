30b9f73f7977ab90f9f5d7df2d571f68afc15e53
jitwei98 <limjitwei@gmail.com>
2018-10-12 16:17:02 +0800

    Temp delete everything

--- README.md
+++
@@ -1,8 +0,0 @@
-#CARPOO
-
-1. Clone into apps/
-
-
-2. Copy the conf/ folder from mappstack/docs/demo or lappstack/docs/demo
-into the same folder as htdocs/
-
--- htdocs/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
- session_start();
-
-  echo "Logout Successfully ";
-  session_destroy();   // function that Destroys Session 
-  header("Location: /carpool/");
-?>
