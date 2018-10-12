c4f20da8bc63839478c52a16330d4e9fc921c150
kelvintankaiboon <e0202641@u.nus.edu>
2018-10-12 17:16:34 +0800

    Fixed login redirection on index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,18 +1,13 @@
 <?php   session_start();  ?>
 <?php
-	if(isset($_SESSION['use']))
-	{
-		// if($_SESSION['use']['isadmin'] == FALSE)
-		// 	header("Location: /carpool/home"); 
-		// include_once ('includes/config.php');
-		// $db = pg_connect($conn_str);
-		// $result = pg_query($db, "SELECT * FROM app_user WHERE email = '$_SESSION['use']'");
-		// $row = pg_fetch_assoc($result);
-		// if ($row['isadmin'] == false) 
+	if(isset($_SESSION['use'])){
+		if (($_SESSION['isadmin']) == "f"){
 			header("Location: /carpool/home"); 
-		// else
-			// header("Location: /carpool/admin_home"); 
-	}	
+		}
+		else{
+			header("Location: /carpool/admin_home"); 
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -48,27 +43,29 @@
 				include_once ('includes/config.php');
 				$db = pg_connect($conn_str);
 				if (isset($_POST['login'])) {
-
 					$email = $_POST['email'];
 					$pword = $_POST['password'];
 					$result = pg_query($db, "SELECT * FROM app_user WHERE email = '$_POST[email]'");
 					$row = pg_fetch_assoc($result);
-
 					if(!$result) {
 						echo "Login Failed!";
 					}
 					else {
 						$phash = $row[password];
-
 						// if (password_verify($pword, $phash)) {
 						if ($pword == $phash) {
 							$_SESSION['use']=$email;
-							// if ($row['isadmin'] == TRUE) {
-								// header("Location: /carpool/admin_home");	
-							// } else {
-								header("Location: /carpool/home");
-							// }
-						} else {
+							$_SESSION['isadmin'] = $row[isadmin];
+							if (($row[isadmin]) == "f") {
+								header("Location: /carpool/home");	
+							} 
+							else {
+								echo $row[isadmin];
+								$_SESSION['isadmin'] = $row[isadmin];
+								header("Location: /carpool/admin_home");
+							}
+						} 
+						else {
 							echo "<p>Incorrect email/password!</p>";
 						}
 					}
