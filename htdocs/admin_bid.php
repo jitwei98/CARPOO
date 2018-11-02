387edf67b6c0641c008767feb7a0577b4e9ac674
jitwei98 <limjitwei@gmail.com>
2018-11-02 15:39:12 +0800

    Add edit for admin page

--- htdocs/admin_bid.php
+++ htdocs/admin_bid.php
@@ -25,15 +25,15 @@
 		<a href="logout.php" style="float:right;padding-top: 45px">Log Out</a>
 	</div>
 	<div class="w3-sidebar w3-bar-block w3-dark-gray" style="width:10%"> 
-		<a href="#" class="w3-bar-item w3-button">app_user</a>
-		<a href="/carpool/admin_bid" class="w3-bar-item w3-button">bid</a>
+		<a href="/carpool/admin_home" class="w3-bar-item w3-button">app_user</a>
+		<a href="#" class="w3-bar-item w3-button">bid</a>
 		<a href="#" class="w3-bar-item w3-button">car</a>
 		<a href="#" class="w3-bar-item w3-button">drive</a>
 		<a href="#" class="w3-bar-item w3-button">offer</a>
 	</div>
 	<div style="margin-left: 10%; margin-top:74px;">
 		<div class="w3-container">
-			<h1>app_user</h1>
+			<h1>bid</h1>
 			<table class="w3-table-all w3-hoverable">
 				<thead>
 					<tr class="w3-light gray">
@@ -45,26 +45,26 @@
 						$user = $_SESSION['use'];
 						include_once ('includes/config.php');
 						$db = pg_connect($conn_str);
-						$result = pg_query($db, "SELECT * FROM app_user;");
+						$result = pg_query($db, "SELECT * FROM bid;");
 
-						$num_user = pg_query($db, "SELECT COUNT(*) FROM app_user;");
-						$num_user_result = pg_fetch_assoc($num_user);
+						$count_result = pg_query($db, "SELECT COUNT(*) FROM bid;");
+						$count_row = pg_fetch_assoc($count_result);
 
 						if (pg_num_rows($result) == 0) {
-							echo "No users currently.<br>";
+							echo "No bids currently.<br>";
 						}
 						else {
 							echo '<h3>';
-					    	echo $num_user_result['count'];//pg_num_rows($result);
-					    	echo " user(s) available";
+					    	echo $count_row['count'];//pg_num_rows($result);
+					    	echo " bid(s) available";
 					    	echo '</h3>';
 					    	echo 	'<th>Actions</th>
-					    	<th>phone_number</th>
-					    	<th>email</th>
-					    	<th>name</th>
-					    	<th>gender</th>
-					    	<th>dob</th>
-					    	<th>password</th>
+					    	<th>date_of_ride</th>
+					    	<th>time_of_ride</th>
+					    	<th>driver</th>
+					    	<th>passenger</th>
+					    	<th>price</th>
+					    	<th>status</th>
 					    	';
 					    }
 					    ?>
@@ -75,14 +75,25 @@
 					while ($row = pg_fetch_assoc($result)) {
 						?>
 						<tr>
-							<td><?php echo '<a href="admin_edit_user.php?edit_id='.$row['email'].'">Edit</a>'.'<a href="admin_delete_user.php?delete_id='.$row['email'].'">Delete</a>'; ?>
+							<td><?php echo '<a href="admin_edit_bid.php?
+							edit_date='.$row['date_of_ride'].'
+							&edit_time='.$row['time_of_ride'].'
+							&edit_driver='.$row['driver'].'
+							&edit_passenger='.$row['passenger'].'
+							">Edit</a>'.'
+							<a href="admin_delete_bid.php?
+							delete_date='.$row['date_of_ride'].'
+							&delete_time='.$row['time_of_ride'].'
+							&delete_driver='.$row['driver'].'
+							&delete_passenger='.$row['passenger'].'
+							">Delete</a>'; ?>
 							</td>
-							<td><?php echo $row['phone_number'];?></td>
-							<td><?php echo $row['email'];?></td>
-							<td><?php echo $row['name'];?></td>
-							<td><?php echo $row['gender'];?></td>
-							<td><?php echo $row['dob'];?></td>
-							<td><?php echo $row['password']?></td>
+							<td><?php echo $row['date_of_ride'];?></td>
+							<td><?php echo $row['time_of_ride'];?></td>
+							<td><?php echo $row['driver'];?></td>
+							<td><?php echo $row['passenger'];?></td>
+							<td><?php echo $row['price'];?></td>
+							<td><?php echo $row['status']?></td>
 						</tr>
 						<?php 
 					}
---
+++ htdocs/admin_edit_bid.php
@@ -0,0 +1,149 @@
+<?php   session_start();  ?>
+<?php
+	// If session is not set or user is not admin then redirect to Login Page
+if(!isset($_SESSION['use'])) 
+{
+	header("Location: /carpool");  
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<style>
+	a {
+		text-decoration: none;
+	}
+	td a { 
+		display: block; 
+	}
+</style>
+</head>
+<body>
+	<div class="w3-container w3-black w3-top">
+		<a href="/carpool/home" style="float:left;"><h1>Car Pooling</h1></a>
+		<a href="logout.php" style="float:right;padding-top: 45px">Log Out</a>
+	</div>
+	<div class="w3-sidebar w3-bar-block w3-dark-gray" style="width:10%"> 
+		<a href="/carpool/admin_home" class="w3-bar-item w3-button">app_user</a>
+		<a href="#" class="w3-bar-item w3-button">bid</a>
+		<a href="#" class="w3-bar-item w3-button">car</a>
+		<a href="#" class="w3-bar-item w3-button">drive</a>
+		<a href="#" class="w3-bar-item w3-button">offer</a>
+	</div>
+	<div style="margin-left: 10%; margin-top:74px;">
+		<div class="w3-container">
+			<h1>bid</h1>
+			<form class="w3-container" method="POST">
+				<table class="w3-table-all w3-hoverable">
+					<?php 
+					include_once ('includes/config.php');
+					$db = pg_connect($conn_str);
+					$result = pg_query($db, "SELECT * FROM bid WHERE 
+						date_of_ride='$_GET[edit_date]' AND 
+						time_of_ride='$_GET[edit_time]' AND
+						driver='$_GET[edit_driver]' AND
+						passenger='$_GET[edit_passenger]'
+						");
+					if (pg_num_rows($result) == 0) {
+						echo "<h6>Bid not found!<br></h6>";
+					} else if (!$result) {
+						echo pg_last_error($db)."<br>";
+					} else {
+						$row = pg_fetch_assoc($result);
+						?>
+						<!-- TODO: Follow index.php -->
+						<tr>
+							<td><label for="date_of_ride"><b>date of ride : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['date_of_ride'].'" name="date_of_ride_updated"></td>'; ?>
+						</tr>
+						<tr>
+							<td><label for="time_of_ride"><b>time of ride : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['time_of_ride'].'" name="time_of_ride_updated"></td>'; ?>
+						</tr>
+						<tr>
+							<td><label for="driver"><b>driver : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['driver'].'" name="driver_updated"></td>'; ?>
+						</tr>
+						<tr>
+							<td><label for="passenger"><b>passenger : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['passenger'].'" name="passenger_updated"></td>'; ?>
+						</tr>
+						<tr>
+							<td><label for="price"><b>price : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['price'].'" name="price_updated"></td>'; ?>
+						</tr>
+						<tr>
+							<td><label for="status"><b>status : </b></label></td>
+							<?php echo '<td><input type="text" value="'.$row['status'].'" name="status_updated"></td>'; ?>
+						</tr>
+				</table>
+				<input type="submit" name="edit" value="Submit">
+				<?php }?>
+			</form>
+
+
+			<!-- Update in database -->
+			<h1>
+				<?php 
+				date_default_timezone_set('Asia/Singapore');
+				$date_curr = date("Y/m/d");
+				$time_curr = date("h/i/sa");
+
+				function isModified() {
+					return !empty($_POST[date_of_ride_updated]) || 
+							!empty($_POST[time_of_ride_updated]) || 
+							!empty($_POST[driver_updated]) || 
+							!empty($_POST[passenger_updated]) ||
+							!empty($_POST[price_updated]) ||
+							!empty($_POST[status_updated])
+							;
+				}
+
+				function update_bid($db, $row) {
+					$date_of_ride = !empty($_POST[date_of_ride_updated]) ? $_POST[date_of_ride_updated] : $row[date_of_ride];
+					$time_of_ride = !empty($_POST[time_of_ride_updated]) ? $_POST[time_of_ride_updated] : $row[time_of_ride];
+					$driver = !empty($_POST[driver_updated]) ? $_POST[driver_updated] : $row[driver];
+					$passenger = !empty($_POST[passenger_updated]) ? $_POST[passenger_updated] : $row[passenger];
+					$price = !empty($_POST[price_updated]) ? $_POST[price_updated] : $row[price];
+					$status = !empty($_POST[status_updated]) ? $_POST[status_updated] : $row[status];
+					
+					$query = "UPDATE bid SET 
+								date_of_ride='$date_of_ride', 
+								time_of_ride='$time_of_ride', 
+								driver='$driver', 
+								passenger='$passenger', 
+								price='$price', 
+								status='$status' 
+								WHERE date_of_ride='$_GET[edit_date]' AND
+										time_of_ride='$_GET[edit_time]' AND
+										driver='$_GET[edit_driver]' AND
+										passenger='$_GET[edit_passenger]'
+								 ;";
+
+					return pg_query($db, $query);
+				}
+				
+				if (!empty($_POST['edit']) && isModified()) {
+					if (update_bid($db, $row)) { // affected rows > 0
+						// echo "Bid successfully updated!<br>";
+						$url = 'Location: /carpool/admin_edit_bid.php?edit_date='.
+								$row['date_of_ride'].'&edit_time='.
+								$row['time_of_ride'].'&edit_driver='.
+								$row['driver'].'&edit_passenger='.
+								$row['passenger'];
+						header($url);
+					} else {
+						echo "Error updating bid!<br>";
+						echo pg_last_error($db)."<br>";
+					}
+				}
+				?>
+			</h1>
+
+
+		</div>
+	</div>
+</body>
+</html>
