b50fa246f7243089e7d7ee2ec517134b48d8fb69
kelvintankaiboon <e0202641@u.nus.edu>
2018-11-09 03:14:17 +0800

    Fixed sizing error of navbar in index.php

--- htdocs/includes/navbar.php
+++ htdocs/includes/navbar.php
@@ -1,25 +1,7 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="height:65px;">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<?php
-				if (isset($_SESSION['use'])){
-					echo '
-					<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="icon icon-bar"></span>
-					</button>';
-				}
-			?>
 			<a href="/carpool/home" class="navbar-brand"> CARPOOL</a>
 		</div>
-		<?php
-			if (isset($_SESSION['use'])){
-				echo '
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php">Log Out</a></li>
-					</ul>
-				</div>';
-			}
-		?>
 	</div>
 </nav>
